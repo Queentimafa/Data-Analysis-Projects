6fc4916ef0ce97dd94407dbc3baeecdd0a76e694
Queentimafa <58369664+Queentimafa@users.noreply.github.com>
2024-08-04 11:27:20 +0000

    Add files via upload

---
+++ DATA CLEANING PROJECT WITH SQL.sql
@@ -0,0 +1,196 @@
+-- Cleaning data in MYSQL
+
+SELECT*
+FROM layoffs;
+
+-- Remove dublicate
+-- Standardize the data
+-- Null values or blank values
+-- Remove blank columns
+
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+
+INSERT layoffs_staging
+SELECT*
+FROM layoffs;
+
+SELECT*
+FROM layoffs_staging;
+
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off, percentage_laid_off, `date`) AS row_num
+FROM layoffs_staging;
+
+WITH duplicate_cte AS 
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, 
+total_laid_off, percentage_laid_off, `date`, stage, 
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging
+)
+SELECT*
+FROM duplicate_cte
+WHERE row_num > 1 ;
+
+-- check for duplicate
+SELECT *
+FROM layoffs_staging
+WHERE company = 'Casper'
+;
+
+WITH duplicate_cte AS 
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, 
+total_laid_off, percentage_laid_off, `date`, stage, 
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging
+)
+SELECT*
+FROM duplicate_cte
+WHERE row_num > 1 ;
+
+-- Got the table information from putting the cursor on the layoffs_staging table name and clicking on the 1st i button 
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SELECT *
+FROM layoffs_staging2;
+
+INSERT INTO layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, 
+total_laid_off, percentage_laid_off, `date`, stage, 
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging;
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE row_num > 1 ;
+
+DELETE 
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging2;
+
+-- Standardizing data
+
+SELECT company, TRIM(company)
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET company = TRIM(company);
+
+-- To view columns
+
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY 1;
+
+-- To check coloumns with similar meaning but different names
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry LIKE 'Crypto%';
+
+-- To change update names 
+
+UPDATE layoffs_staging2
+SET industry ='Crypto'
+WHERE industry LIKE 'Crypto%';
+
+SELECT DISTINCT industry
+FROM layoffs_staging2;
+
+SELECT DISTINCT country
+FROM layoffs_staging2
+ORDER BY 1;
+
+SELECT DISTINCT country, TRIM(TRAILING '.' FROM country) 
+FROM layoffs_staging2
+ORDER BY 1;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country) 
+WHERE COUNTRY LIKE 'United States%';
+
+SELECT `Date`
+FROM layoffs_staging2;
+
+SELECT `date`,
+STR_TO_DATE (`date`, '%m/%d/%Y')
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE (`date`, '%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2
+MODIFY `date` DATE;
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+UPDATE layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+
+SELECT *
+FROM layoffs_staging2
+WHERE  industry IS NULL
+OR industry = '';
+
+SELECT t1.industry, t2.industry
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND (t2.industry IS NOT NULL);
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL 
+AND t2.industry IS NOT NULL;
+
+SELECT *
+FROM layoffs_staging2
+WHERE company = 'Airbnb';
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+DELETE
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
